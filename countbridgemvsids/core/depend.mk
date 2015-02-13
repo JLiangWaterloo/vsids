5181c1620cd7b41b4a9c94e680d75bf029b4476f
JLiangWaterloo <paradoxical.reasoning@gmail.com>
2015-02-12 21:17:54 -0500

    count bridge mvsids

---
+++ countbridgemvsids/core/depend.mk
@@ -0,0 +1,27 @@
+/home/jliang/sat/Benchmarks/countbridgemvsids/core/Main.o /home/jliang/sat/Benchmarks/countbridgemvsids/core/Main.or /home/jliang/sat/Benchmarks/countbridgemvsids/core/Main.od /home/jliang/sat/Benchmarks/countbridgemvsids/core/Main.op: /home/jliang/sat/Benchmarks/countbridgemvsids/core/Main.cc \
+ /home/jliang/sat/Benchmarks/countbridgemvsids/utils/System.h \
+ /home/jliang/sat/Benchmarks/countbridgemvsids/mtl/IntTypes.h \
+ /home/jliang/sat/Benchmarks/countbridgemvsids/utils/ParseUtils.h \
+ /home/jliang/sat/Benchmarks/countbridgemvsids/utils/Options.h \
+ /home/jliang/sat/Benchmarks/countbridgemvsids/mtl/Vec.h \
+ /home/jliang/sat/Benchmarks/countbridgemvsids/mtl/XAlloc.h \
+ /home/jliang/sat/Benchmarks/countbridgemvsids/core/Dimacs.h \
+ /home/jliang/sat/Benchmarks/countbridgemvsids/core/SolverTypes.h \
+ /home/jliang/sat/Benchmarks/countbridgemvsids/mtl/Alg.h \
+ /home/jliang/sat/Benchmarks/countbridgemvsids/mtl/Map.h \
+ /home/jliang/sat/Benchmarks/countbridgemvsids/mtl/Alloc.h \
+ /home/jliang/sat/Benchmarks/countbridgemvsids/core/Solver.h \
+ /home/jliang/sat/Benchmarks/countbridgemvsids/mtl/Heap.h
+/home/jliang/sat/Benchmarks/countbridgemvsids/core/Solver.o /home/jliang/sat/Benchmarks/countbridgemvsids/core/Solver.or /home/jliang/sat/Benchmarks/countbridgemvsids/core/Solver.od /home/jliang/sat/Benchmarks/countbridgemvsids/core/Solver.op: /home/jliang/sat/Benchmarks/countbridgemvsids/core/Solver.cc \
+ /home/jliang/sat/Benchmarks/countbridgemvsids/mtl/Sort.h \
+ /home/jliang/sat/Benchmarks/countbridgemvsids/mtl/Vec.h \
+ /home/jliang/sat/Benchmarks/countbridgemvsids/mtl/IntTypes.h \
+ /home/jliang/sat/Benchmarks/countbridgemvsids/mtl/XAlloc.h \
+ /home/jliang/sat/Benchmarks/countbridgemvsids/core/Solver.h \
+ /home/jliang/sat/Benchmarks/countbridgemvsids/mtl/Heap.h \
+ /home/jliang/sat/Benchmarks/countbridgemvsids/mtl/Alg.h \
+ /home/jliang/sat/Benchmarks/countbridgemvsids/utils/Options.h \
+ /home/jliang/sat/Benchmarks/countbridgemvsids/utils/ParseUtils.h \
+ /home/jliang/sat/Benchmarks/countbridgemvsids/core/SolverTypes.h \
+ /home/jliang/sat/Benchmarks/countbridgemvsids/mtl/Map.h \
+ /home/jliang/sat/Benchmarks/countbridgemvsids/mtl/Alloc.h
